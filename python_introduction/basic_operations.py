9ab408046ee6b8895ec40cc6d3010d9662c1f353
Habiba Haroun <22habibaabdou23@gmail.com>
2024-12-23 00:01:22 +0200

    Update basic_operations.py

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,1 +1,11 @@
+# basic_operations.py
 
+
+# Define variabls
+mumber1 = 10
+number2 = 5
+
+# calculations
+Addition = number1 + number2
+Subtraction = number1 - number2
+Multiplication = number1 + number2
